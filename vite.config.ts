e04ce85700e469405688b406281e1b35ab34a2e6
Archit Gupta <architg564@gmail.com>
2026-04-25 23:29:52 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
       outDir: "dist",
       tsconfigPath: "./tsconfig.app.json",
     }),
+    cssInjectedByJsPlugin(),
   ],
   build: {
     lib: {
